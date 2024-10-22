883dc35f9bde457aa000e97b081f39d85a153f0a
Alejo Carobolante <alejocarobolante@gmail.com>
2024-10-22 18:56:23 -0300

    se crearon los PopUps

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
 import { renderCategories } from "./services/categories.js";
-import './style.css';
+import "./style.css"
 renderCategories();
+
+//POPUP
+const buttonAdd = document.getElementById("buttonAdd")
+buttonAdd.addEventListener('click', ()=>{
+    openModal();
+})
+const cancelButton = document.getElementById("cancelButton")
+cancelButton.addEventListener('click', ()=>{
+    HandleCancelButton();
+})
+
+const HandleCancelButton = ()=>{
+    closenModal();
+}
+
+//Funciones abrir-cerrar
+const openModal = ()=>{
+    const modal = document.getElementById("modalPopUp");
+    modal.style.display = 'flex'
+}
+
+const closenModal = ()=>{
+    const modal = document.getElementById("modalPopUp");
+    modal.style.display = 'none'
+}
+
+//Guardar
+
+const acceptButton = document.getElementById("acceptButton")
+acceptButton.addEventListener('click', ()=>{
+    HandleSaveOrModify();
+})
+
+const HandleSaveOrModify = ()=>{
+const name = document.getElementById("name").value
+const img = document.getElementById("img").value
+const price = document.getElementById("price").value
+}
--- services/categories.js
+++ services/categories.js
@@ -1,28 +1,35 @@
-export const renderCategories = ()=>{
+export const renderCategories = () => {
     const ulList = document.getElementById("listFilter");
     ulList.innerHTML = `
-    <li id = "Todo">Todos los productos</li>
-    <li id = "Hamburguesas">Hamburguesas</li>
-    <li id = "Papas">Papas</li>
-    <li id = "Gaseosas">Bebidas</li>
-    <li id = "mayorPrecio">Precio: de mayor a menor</li>
-    <li id = "menorPrecio">Precio: de menor a mayor</li>
+        <li id="Todo">Todos los productos</li>
+        <li id="Hamburguesas">Hamburguesas</li>
+        <li id="Papas">Papas</li>
+        <li id="Gaseosas">Bebidas</li>
+        <li id="mayorPrecio">Precio: de mayor a menor</li>
+        <li id="menorPrecio">Precio: de menor a mayor</li>
     `;
+
     const liElements = ulList.querySelectorAll("li");
-    liElements.forEach((liElement)=>{
-        liElement.addEventListener("click",()=>{
-            handleClick(liElement);
+
+    // Añadimos el listener a cada li
+    liElements.forEach((liElement) => {
+        liElement.addEventListener("click", () => {
+            handleClick(liElement); // Pasamos el elemento clicado
         });
     });
-    const handleClick = (elemento)=> {
-        liElements.forEach((el)=>{
-            if (el.classList.contains('liActive')){
+
+    const handleClick = (elemento) => {
+        // Verificar si el elemento ya tiene la clase 'liActive'
+        if (elemento.classList.contains('liActive')) {
+            // Si la tiene, la removemos (deseleccionar)
+            elemento.classList.remove('liActive');
+        } else {
+            // Si no la tiene, removemos la clase de todos los demás elementos
+            liElements.forEach((el) => {
                 el.classList.remove('liActive');
-            }else{
-                if(elemento === el){
-                    el.classList.add('liActive');
-                }
-            }
-        })
-    }
+            });
+            // Y agregamos la clase al elemento clicado (seleccionar)
+            elemento.classList.add('liActive');
+        }
+    };
 };
